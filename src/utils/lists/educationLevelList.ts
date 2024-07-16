86d44e0b4045d9b32a513bfe82b6f042bf0971b1
Wong Jin Jye <39756986+jinjinjinjye@users.noreply.github.com>
2024-07-17 07:10:13 +0800

    added poly and ITE diploma as options

--- src/utils/lists/educationLevelList.ts
+++ src/utils/lists/educationLevelList.ts
@@ -3,6 +3,8 @@ export const educationLevelList = [
   "PSLE",
   "GCE N / O-Levels",
   "GCE A-Levels",
+  "ITE Diploma",
+  "Polytechnic Diploma",
   "Bachelor's Degree",
   "Master's Degree",
   "Ph.D.(Doctor of Philosophy) / Doctorate",
